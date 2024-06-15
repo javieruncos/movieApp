73bd2a3d188a6a19e8c4f71e2d4a0c682580d0b3
Javier Uncos <javiersrfc30@gmail.com>
2024-06-15 14:53:23 -0300

    agregamos carga de spinner en el modal peliculas

--- src/components/Uicomponents/ModalMovie.jsx
+++ src/components/Uicomponents/ModalMovie.jsx
@@ -2,15 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import '../../assets/style/ModalMovie.css';
 import { getModalId, getMovie } from '../../helpers/app';
+import SpinnerModal from './SpinnerModal';
 const ModalMovie = ({ lgShow, setLgShow, movieId }) => {
     const [movie, setMovie] = useState({})
     const [arregloSugerencia, setArregloSugerencia] = useState([])
+    const [mostrarSpinner, setMostrarSpinner] = useState(true)
     const categoria = 'movie'
 
     useEffect(() => {
+        setMostrarSpinner(true);
         getModalId(movieId).then((res) => {
             setMovie(res)
             console.log(res)
+            const timeoutID = setTimeout(() => {
+                setMostrarSpinner(false);
+            }, 1000);
+
+            return () => clearTimeout(timeoutID);
+
         })
     }, [movieId])
 
@@ -31,26 +40,35 @@ const ModalMovie = ({ lgShow, setLgShow, movieId }) => {
                 className='modal-movie'
             >
                 <Modal.Header closeButton className='header-modal'>
+
                     <div className='modal-contenedor-img'>
-                        <div className='modal-img'>
-                            <img src={movie.Poster} alt="" />
-                        </div>
-                        <div className='container modalcontenido-movie'>
-                            <h3 className='text-light'>{movie.Title}</h3>
-                            <div className='info-adicional'>
-                                <span>{movie.Type}</span>
-                                <span>{movie.Year}</span>
-                                <span>{movie.Runtime}</span>
-                                <span>{movie.imdbRating}</span>
-                            </div>
-                            <p>
-                                {movie.Plot}
-                            </p>
-                            <div>
-                                <h5>elenco</h5>
-                                <p>{movie.Actors}</p>
-                            </div>
-                        </div>
+                        {mostrarSpinner === true ? (
+                            <SpinnerModal></SpinnerModal>
+                        ) :
+                            <>
+                                <div className='modal-img'>
+                                    <img src={movie.Poster} alt="" />
+                                </div>
+                                <div className='container modalcontenido-movie'>
+                                    <h3 className='text-light'>{movie.Title}</h3>
+                                    <div className='info-adicional'>
+                                        <span>{movie.Type}</span>
+                                        <span>{movie.Year}</span>
+                                        <span>{movie.Runtime}</span>
+                                        <span>{movie.imdbRating}</span>
+                                    </div>
+                                    <p>
+                                        {movie.Plot}
+                                    </p>
+                                    <div>
+                                        <h5>elenco</h5>
+                                        <p>{movie.Actors}</p>
+                                    </div>
+                                </div>
+                            </>
+
+                        }
+
                     </div>
                 </Modal.Header>
                 <Modal.Body>
@@ -58,10 +76,10 @@ const ModalMovie = ({ lgShow, setLgShow, movieId }) => {
                     <div className='modal-contenido container'>
                         <div className='row'>
                             {
-                                arregloSugerencia.slice(0,9).map((movie) => (
+                                arregloSugerencia.slice(0, 9).map((movie) => (
                                     <div className='col-6 col-md-6 col-lg-4' key={movie.imdbID}>
                                         <div className='modal-movie-card my-2'>
-                                           <img src={movie.Poster} alt="" className='w-100'/>
+                                            <img src={movie.Poster} alt="" className='w-100' />
                                         </div>
                                     </div>
                                 ))
